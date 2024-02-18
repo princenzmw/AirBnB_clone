c26c5d5cbc4fea7508087c336369469a4d895413
princenzmw <princenzmw@gmail.com>
2024-02-18 23:47:34 +0200

    Changed the text that were causing The test to fail because the test_help_update function was comparing the expected help message h with the actual output from the do_update function, but they did not match.

--- tests/test_console.py
+++ tests/test_console.py
@@ -98,11 +98,11 @@ class TestHBNBCommand_help(unittest.TestCase):
 
     def test_help_update(self):
         h = (
-            "Usage: update <class> <id> <attribute_name> <attribute_value> or"
-            "\n       <class>.update(<id>, <attribute_name>, <attribute_value"
-            ">) or\n       <class>.update(<id>, <dictionary>)\n        "
-            "Update a class instance of a given id by adding or updating\n   "
-            "     a given attribute key/value pair or dictionary."
+            """Usage: update <class> <id> <attribute_name> <attribute_value> or
+        <class>.update(<id>, <attribute_name>, <attribute_value>) or
+        <class>.update(<id>, <dictionary>)
+        Update a class instance of a given id by adding or updating
+        a given attribute key/value pair or dictionary."""
         )
         with patch("sys.stdout", new=StringIO()) as output:
             self.assertFalse(HBNBCommand().onecmd("help update"))
